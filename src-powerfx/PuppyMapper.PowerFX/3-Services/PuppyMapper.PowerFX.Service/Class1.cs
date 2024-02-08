2793c1fb2c463e97be7af0d4a300ef0a6b38b2dc
The Juanito Learns Show <98781316+TheJuanitoLearnsShow@users.noreply.github.com>
2024-02-08 17:13:22 -0500

    Powerfx structure

---
+++ src-powerfx/PuppyMapper.PowerFX/3-Services/PuppyMapper.PowerFX.Service/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace PuppyMapper.PowerFX.Service
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ src-powerfx/PuppyMapper.PowerFX/7-Tests/PuppyMapper.PowerFX.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace PuppyMapper.PowerFX.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
