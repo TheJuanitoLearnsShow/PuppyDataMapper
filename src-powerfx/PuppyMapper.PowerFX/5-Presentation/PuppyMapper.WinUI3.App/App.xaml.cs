d0c2201c401b86f6345fe3afbe65457163de762c
The Juanito Learns Show <98781316+TheJuanitoLearnsShow@users.noreply.github.com>
2025-07-09 22:05:51 -0400

    WinUI3 skeleton

--- src-powerfx/PuppyMapper.PowerFX/5-Presentation/PuppyMapper.WinUI3.App/App.xaml.cs
+++ src-powerfx/PuppyMapper.PowerFX/5-Presentation/PuppyMapper.WinUI3.App/App.xaml.cs
@@ -19,7 +19,7 @@ using Windows.Foundation.Collections;
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
-namespace PuppyMapper.WinUI.App
+namespace PuppyMapper.WinUI3.App
 {
     /// <summary>
     /// Provides application-specific behavior to supplement the default Application class.
